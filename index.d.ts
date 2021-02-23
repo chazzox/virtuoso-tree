07bc3ff75f091a2d35b84cb70df48337aed94112
chazzox <chazzox@users.noreply.github.com>
2021-02-23 20:35:15 +0000

    deploy: 37482e5f5e0affebd6a9b1006e50e02180146738

--- index.d.ts
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-interface ListPropTypes {
-    itemHeight?: number;
-    endReached?: (num: number) => any;
-    dataGenerator: Generator<any, any, any>;
-    style?: React.CSSProperties;
-}
-export declare const FixedSizeTree: React.FC<ListPropTypes>;
-export {};
