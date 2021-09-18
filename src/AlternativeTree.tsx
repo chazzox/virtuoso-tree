cdd161e763fafb6ef0287c8710110eb584a8c0d6
chazzox <caylott@outlook.com>
2021-09-18 18:27:49 +0100

    fleshing out the compute function

--- src/AlternativeTree.tsx
+++ src/AlternativeTree.tsx
@@ -1,40 +1,43 @@
 import * as React from 'react';
-import mergeRefs from 'react-merge-refs';
 import { Virtuoso, VirtuosoHandle } from 'react-virtuoso';
 
-/* TODO:
-	(1): Learn how to use generics in typescript (whoops)
-		- https://www.typescriptlang.org/docs/handbook/utility-types.html
-	(2): Add header and footer components
-	(3): Add customisable collapse handles (with optional defaults)
-*/
+interface NodeType {
+	data: {};
+	nestingLevel: number;
+}
 
 interface AltTreeComponentProps {
-	treeWalker: Generator;
-	/**
-	 * If you can guarantee the height of all elements then set this variable, it will increase performance as virtuoso tree will not have to measure the content
-	 */
+	treeWalker: Generator<NodeType | undefined, undefined, NodeType>;
+	/** If you can guarantee the height of all elements then set this variable, it will increase performance as virtuoso tree will not have to measure the content */
 	itemHeight?: number;
-	/**
-	 * Enables chunking by n number of pixels
-	 */
+	/** Enables chunking by n number of pixels */
 	overscan?: number;
-	/**
-	 * The component that is used by Virtuoso to render each item
-	 */
+	/** The component that is used by Virtuoso to render each item */
 	itemComponent: React.ReactNode;
 	placeholder?: React.ReactNode;
 
 	listRef?: React.Ref<VirtuosoHandle>;
 }
 
-export const AlternativeTree: React.FC<AltTreeComponentProps> = ({ treeWalker, placeholder, listRef }) => {
-	const list = React.useRef<VirtuosoHandle>(null);
+/**
+ * What does this component need to do?
+ * compute node values for generator function
+ * add ability to update values on a node
+ * memoise computations (maybe use the useMemo hook?)
+ */
+
+interface ListObject {
+	nestingLevel: number;
+}
+
+// @ts-expect-error
+const createRecord = ({ nestingLevel }: ListObject) => ({ nestingLevel: nestingLevel });
+
+export const AlternativeTree: React.FC<AltTreeComponentProps> = ({ treeWalker, placeholder }) => {
 	// internal state/variables
 
+	// @ts-expect-error
 	const [order] = React.useState([]);
-
-	const [attachRefs, updateAttachRefs] = React.useState<React.Ref<VirtuosoHandle>>(mergeRefs([list, listRef || null]));
 
 	// @ts-expect-error
 	const [records] = React.useState([]);
@@ -44,8 +47,28 @@ export const AlternativeTree: React.FC<AltTreeComponentProps> = ({ treeWalker, p
 	// @ts-expect-error
 	const reComputeTree = () => {};
 
-	// @ts-expect-error
-	const computeTree = () => {};
+	const computeTree = () => {
+		// @ts-expect-error
+		const iter = treeWalker();
+		const { value: root } = iter.next();
+		let currentRecord,
+			// @ts-expect-error
+			tempRecord = root;
+
+		let isTraversingRoot = true;
+		console.log(currentRecord);
+
+		while (currentRecord !== null) {
+			console.dir(currentRecord);
+			const { value: next } = iter.next();
+			if (next === undefined) {
+				if (isTraversingRoot) {
+					isTraversingRoot = false;
+				}
+			}
+			currentRecord = next;
+		}
+	};
 
 	// @ts-expect-error
 	const getRecordData = () => {};
@@ -53,25 +76,9 @@ export const AlternativeTree: React.FC<AltTreeComponentProps> = ({ treeWalker, p
 	// @ts-expect-error
 	const getItemData = () => {};
 
-	// @ts-expect-error
-	const scrollTo = (scrollOffset: number) => {
-		list.current?.scrollTo({ top: scrollOffset });
-	};
-
-	// @ts-expect-error
-	const scrollToIndex = (id: string, align: any) => {
-		// @ts-expect-error
-		list.current?.scrollToIndex({ align: 'start', index: order!.indexOf(id), behavior: 'smooth' });
-	};
-
-	// React lifecycles
 	React.useEffect(() => {
-		updateAttachRefs(mergeRefs([list, listRef || null]));
-	}, [listRef]);
+		computeTree();
+	}, []);
 
-	React.useEffect(() => {
-		console.log('treeWalker init');
-	}, [treeWalker]);
-
-	return placeholder && order!.length === 0 ? <>{placeholder}</> : <Virtuoso ref={attachRefs} />;
+	return true ? <>{placeholder}</> : <Virtuoso />;
 };
